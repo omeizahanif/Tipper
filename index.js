807fc09b60320c8d95cbf614537d27f84448d8fa
Hanif Adams <omeizahanif@gmail.com>
2021-11-10 20:45:44 +0100

    created active state function

--- index.js
+++ index.js
@@ -10,8 +10,13 @@ const tipPerPerson = document.getElementById("tip_person");
 const totalPerPerson = document.getElementById("total_person");
 const resetBtn = document.getElementById("reset");
 const tipCategory = document.getElementsByClassName("tip-category")[0];
-console.log(tipCategory.children);
 
+/**
+ * a function to calculate the tip amount,
+ * the tip amount per person and the total
+ * bill per person.
+ * @param {number} tipPercent 
+ */
 function tipCalculator(tipPercent) {
     let bill = parseFloat(billInput.value);
     let numberOfPeople = parseInt(numPeople.value);
@@ -21,44 +26,60 @@ function tipCalculator(tipPercent) {
         tipPerPerson.textContent = `$${tipForEachPerson.toFixed(2)}`;
         totalPerPerson.textContent = `$${((bill/numberOfPeople) + tipForEachPerson).toFixed(2)}`;
     } else {
-        return false;
+        alert("Number of people can't be zero")
     }
-    
     
     return true;
 }
 
+function setActiveState(id) {
+    const tipSelectors = [...tipCategory.children];
+    for (let tipSelector of tipSelectors) {
+        tipSelector.classList.remove("active");
+    }
+    id.classList.add("active");
+}
 
 five.addEventListener("click", function(e) {
-    tipCategory.children.forEach(function(child) {
-        child.classList.remove("active");
-        five.classList.add("active");
-    })
-    
-    tipCalculator(5);
+    setActiveState(five);
+    //tipCalculator(5);
 })
 
 ten.addEventListener("click", function(e) {
-    tipCalculator(10);
+    setActiveState(ten);
+    //tipCalculator(10);
 })
 
 fifteen.addEventListener("click", function(e) {
-    tipCalculator(15);
+    setActiveState(fifteen);
+    //tipCalculator(15);
 })
 
 twentyFive.addEventListener("click", function(e) {
-    tipCalculator(25);
+    setActiveState(twentyFive);
+    //tipCalculator(25);
 })
 
 fifty.addEventListener("click", function(e) {
-    tipCalculator(50);
+    setActiveState(fifty);
+    //tipCalculator(50);
 })
 
 customInput.addEventListener("input", function(e) {
     let customValue = parseFloat(customInput.value);
-    tipCalculator(customValue);
+    setActiveState(customInput);
+    //tipCalculator(customValue);
 })
 
+/*numPeople.addEventListener("input", function(e) {
+
+})*/
+
+resetBtn.addEventListener("click", function(e) {
+    billInput.value = "";
+    customInput.value = "";
+    numPeople.value = "";
+})
 
 
 
